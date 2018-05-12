7a561cede7a7ba63f0a9d6a9ed22248045083851
sajjadhussain123 <33265360+sajjadhussain123@users.noreply.github.com>
2018-05-13 00:05:23 +0500

    Update .env.example.ps1

--- .env.example.ps1
+++ .env.example.ps1
@@ -1,7 +1,7 @@
-$Env:TWILIO_ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-$Env:TWILIO_AUTH_TOKEN = "your_auth_token"
-$Env:TWILIO_TWIML_APP_SID = "APXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-$Env:TWILIO_CALLER_ID = "+1XXXYYYZZZZ"
+$Env:TWILIO_ACCOUNT_SID = "AC145919653f545cad5e81f4389e993eaa"
+$Env:TWILIO_AUTH_TOKEN = "9fe982d33b9b0af3ac2257edc23b972d"
+$Env:TWILIO_TWIML_APP_SID = "AP602df5484376401cb4d1425cfaf40ab1"
+$Env:TWILIO_CALLER_ID = "+12013542122"
 
 # Uncomment the following if you'd like the environment variables
 # to be permanently set on your user account for this machine.
